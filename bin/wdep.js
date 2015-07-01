f84b15929ab82ddb35fba88f9b3dc134d41f2943
Olivier Palta <palta@work.co>
2015-06-30 23:11:29 -0400

    Refactor

--- bin/wdep.js
+++ bin/wdep.js
@@ -1,80 +1,71 @@
 #!/usr/bin/env node
 
+var colors = require('colors/safe');
+var async = require('async');
 var npm = require('npm');
-var path = require('path');
 var fs = require('fs');
 var argv = require('yargs').argv;
 var Table = require('cli-table');
- 
-var table = new Table({
-    head: ['name', 'description'], 
-    colWidths: [20, 80]
-});
-
-npm.load(start);
+var exec = require('sync-exec')
 
 function start(err, npm) {
-  // resolve filenames
-  var filename = (argv._.length) ? argv._[0] : 'package.json';
-  var file = path.join(process.cwd(), filename);
-
-  // attempt to read the file
-  fs.readFile(file, 'utf8', function(err, packageFile) {
-    if (err) {
-      return console.error(err + '\n');
+  try {
+    var json = getPackageJson();
+  } catch (e) {
+    console.error(colors.red('Could not find package.json!'));
+    process.exit(1);
+  }
+  var deps = Object.keys(json.dependencies || {});
+  getRegistryData(deps, function(err, data) {
+    if(err) {
+     console.error(colors.red('Error fetching data from npm registry!'));
+      process.exit(1);
+    } else {
+      renderTable(data);
     }
-
-    var pkg = JSON.parse(packageFile);
-
-    var modules = Object.keys(pkg.dependencies);
-    var devDependencies = Object.keys(pkg.devDependencies) || [];
-
-    // if dev flag is set, include devDependencies
-    if (argv.dev) {
-      modules = dependencies.concat(devDependencies);
-    }
-
-    buildTable(modules);
   });
 }
 
-function buildTable(modules) {
-  var count = modules.length;
+function renderTable(data) {
+  var table = new Table({
+    head: ['name', 'description'], 
+    colWidths: [20, 80]
+  });
+  data.forEach(function(a) {
+    var module = a[Object.keys(a)];
+    table.push([module.name, module.description]);
+  });
+  table.sort(sortByNameAscending);
+  console.log(table.toString());
+}
 
-  function queryNpm(module) {
-    // query npm for module
-    npm.commands.view([module, 'name', 'description'], true, _handleModule);
-  }
-
-  function _handleModule(err, module) {
-    Object.keys(module).forEach(function(index) { 
-      table.push([module[index].name, module[index].description]);
-    });
-
-    count--;
-
-    // once all modules are retrie
-    if (count === 0) {
-      table.sort(sortByNameAscending);
-      displayTable();
+function getRegistryData(arr, cb) {
+  npm.load(function(err) {
+    if(err) {
+      return cb(err);
     }
-  }
+    async.map(arr, function(moduleName, done) {
+      npm.commands.view([moduleName, 'name', 'description'], true, done);
+    }, cb);
+  });
+}
 
-  modules.forEach(queryNpm);
+function getPackageJson() {
+  var root = exec('npm root').stdout;
+  var bits = root.split('/');
+  bits[bits.length - 1] = 'package.json'
+  var packageJsonPath = bits.join('/');
+  return JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
 }
 
 function sortByNameAscending(elemA, elemB) {
   if (elemA[0] > elemB[0]) {
     return 1;
   } 
-
   if (elemA[0] < elemB[0]) {
     return -1;
   }
-
   return 0;
 }
 
-function displayTable() {
-  console.log(table.toString());
-}
+start();
